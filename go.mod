3cd218e44a671ac4b17df643bf9804cca59cc15b
Vovko <vkouzin@outlook.com>
2024-07-07 13:08:17 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/byvko-dev/legal
+
+go 1.22.3
+
+require github.com/a-h/templ v0.2.747
+
+require github.com/pkg/errors v0.9.1 // indirect
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/a-h/templ"
+)
+
+//go:generate templ generate
+
+var serviceWebsiteURL = "https://byvko.dev"
+var lastUpdatedDate = fmt.Sprint(time.Date(2024, 07, 07, 0, 0, 0, 0, time.UTC).Format("Jan _2 2006"))
+
+func main() {
+	err := renderToFile(PrivacyPolicy(), "privacy-policy", true)
+	if err != nil {
+		panic(err)
+	}
+	err = renderToFile(TermsOfService(), "terms-of-service", true)
+	if err != nil {
+		panic(err)
+	}
+	err = renderToFile(Directory(), "index", true)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func renderToFile(component templ.Component, name string, withPartial bool) error {
+	{
+		f, err := os.Create(fmt.Sprintf("build/%s.html", name))
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		err = Page(component).Render(context.Background(), f)
+		if err != nil {
+			return err
+		}
+	}
+	if withPartial {
+		f, err := os.Create(fmt.Sprintf("build/%s-partial.html", name))
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		err = component.Render(context.Background(), f)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
